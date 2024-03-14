719da3e340ef4862c0d2d86f6ce7b6cb6573fabd
Nadir0glu <nadiroglunail@gmail.com>
2024-03-14 00:44:33 -0400

    nail

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word):
+        matched_words = []
+        for _ in word:
+            if sorted(_.lower()) == sorted(self.word.lower()):
+                matched_words.append(_)
+        return matched_words
+
+
+    
